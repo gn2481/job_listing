c34ae5a219f7874a5e6fce8b34dc73f1cd25c630
yuan <gn2481@gmail.com>
2020-12-16 10:31:48 +0800

    authenticate_user_in_jobs_controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_action :find_job, only: %i[show edit update destroy]
+  before_action :authenticate_user!, only: %i[new create edit update destroy]
   def index 
     @jobs = Job.all
   end
