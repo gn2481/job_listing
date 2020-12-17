4662354e0e4c3b16c0038736daf40685dc314cbb
yuan <gn2481@gmail.com>
2020-12-17 12:43:57 +0800

    add_resumes_view

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,19 +1,14 @@
 class ResumesController < ApplicationController
-  before_action :find_resume, only:%i[show edit update destroy ]
+  before_action :authenticate_user!
+  before_action :find_job, only:%i[new create]
 
-  def index
-    @resumes = Resume.all
-  end
-
-  def show
-  end
-  
   def new
     @resume = Resume.new
   end
   
   def create
-    @resume = Resume.new(resume_params)
+    @resume = @job.resumes.new(resume_params)
+    @resume.user = current_user
     if @resume.save
       redirect_to root_path, notice: "成功投遞履歷"
     else
@@ -21,33 +16,12 @@ class ResumesController < ApplicationController
     end
   end
   
-  def edit
-  end
-
-  def update
-    @resume = Resume.update(resume_params)
-    if @resume.save
-      redirect_to root_path, notice: "成功修改履歷"
-    else
-      render :edit , notice: "請再試一次！"
-    end
-  end
-
-  def destroy
-    if @resume.destroy
-      redirect_to root_path, notice: "成功刪除履歷"
-    else
-      redirect_to root_path, notice: "請再試一次"
-    end
-  end
-  
-
   private
-  def find_resume
-    @resume = Resume.find(params[:id])
+  def find_job
+    @job = Job.find(params[:job_id])
   end
 
   def resume_params
-    params.require(:resume).permit()
+    params.require(:resume).permit(:cover_letter, :attachment)
   end
 end
